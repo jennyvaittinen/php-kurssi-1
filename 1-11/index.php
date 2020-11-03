dc98448ea43e6b4e772664c1b54b994cd1801e19
jennyvaittinen <73814101+jennyvaittinen@users.noreply.github.com>
2020-11-03 10:27:05 +0200

    Update index.php

--- 1-11/index.php
+++ 1-11/index.php
@@ -1,1 +1,10 @@
 <?php
+$muuttuja = 6097;
+if($muuttuja>4000){
+echo "A";}
+elseif($muuttuja==0 || $muuttuja==1000){
+echo "B";}
+elseif($muuttuja%2==0 ||$muuttuja<3000){
+echo "C";}
+else{
+echo "D";}
